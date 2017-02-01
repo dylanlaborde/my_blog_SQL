f642bef908ef0866550afbd6120408a464dccb0a
Dylan <dylan4047@gmail.com>
2017-02-01 15:43:25 +0100

    affichage des information

--- index.php
+++ index.php
@@ -3,23 +3,34 @@ require 'vendor/autoload.php';
 ORM::configure('mysql:host=localhost;dbname=my_blog');
 ORM::configure('username', 'root');
 ORM::configure('password', 'root');
+$posts = ORM::for_table('posts')->find_many();
 ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
+<title>My_Blog</title>
 <h1>mon blog</h1>
+
+<h4>article ajoutée</h4>
+<a href="/form.php">
+<button>Ajouter un article</button>
+	
+</a>
+
 <table class="ui celled table">
 	<thead>
 		<tr>
 			<th>Titre</th>
 			<th>contenue</th>
-			<th>Nom de l'auteur / date Création</th>
+			<th>ajoutée par</th>
 		</tr>
 	</thead>
 	<tbody>
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-		</tr>		
+		<?php foreach ($posts as $key): ?>	
+			<tr>
+				<td><?= $key->title;?></td>
+				<td><?= $key->content;?></td>
+				<td><?= $key->author."\n"."le"."\n".$key->created_at?></td>
+			</tr>		
+		<?php endforeach ?>
 	</tbody>
 </table>
 
