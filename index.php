beb3da707e77521c600c62ee93b275044024903d
Dylan <dylan4047@gmail.com>
2017-02-01 15:02:50 +0100

    creation table et ajout semantic

--- index.php
+++ index.php
@@ -4,5 +4,22 @@ ORM::configure('mysql:host=localhost;dbname=my_blog');
 ORM::configure('username', 'root');
 ORM::configure('password', 'root');
 ?>
-<h1>Mon Blog</h1>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
+<h1>mon blog</h1>
+<table class="ui celled table">
+	<thead>
+		<tr>
+			<th>Titre</th>
+			<th>contenue</th>
+			<th>Nom de l'auteur / date Création</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>		
+	</tbody>
+</table>
 
