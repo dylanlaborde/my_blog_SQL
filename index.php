e922b8d0461631dc7c818e78e15e657f350f83dd
Dylan <dylan4047@gmail.com>
2017-02-01 14:30:20 +0100

    ajout config ORM

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<h1>Mon Blog</h1>
 <?php 
+require 'vendor/autoload.php';
+ORM::configure('mysql:host=localhost;dbname=my_blog');
+ORM::configure('username', 'root');
+ORM::configure('password', 'root');
+?>
+<h1>Mon Blog</h1>
 
- ?>
