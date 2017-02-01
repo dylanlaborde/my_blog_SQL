dfd7d1dd2014726db377433ab80a760684391c20
Dylan <dylan4047@gmail.com>
2017-02-01 16:32:24 +0100

    ajout des condition du formulaire

--- form.php
+++ form.php
@@ -1,6 +1,21 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
 <h1>form</h1>
-<form action="/submit_post.php" method="POST">
+<?php 
+
+if (isset($_POST['title']) && isset($_POST['author']) && isset($_POST['content']) && !empty($_POST['title']) && !empty($_POST['author']) && !empty($_POST['content'])) {
+	header('Location: http://0.0.0.0:4002/');
+	exit();
+}else{?>
+
+<form action="/form.php" method="POST">
+	
 	<div>
+		<?php if (isset($_POST['title']) && isset($_POST['author']) && isset($_POST['content']) && empty($_POST['title']) && empty($_POST['author']) && empty($_POST['content'])): ?>
+			<div class="ui error message">
+				<div class="header">Erreur</div>
+				<p>veuiller rensegner les champs</p>
+			</div>
+		<?php endif ?>
 		<div>
 			<label for="title">titre</label>
 			<input type="text" name="title">
@@ -13,7 +28,11 @@
 		<div>
 			<textarea name="content" id="content" cols="30" rows="10"></textarea>
 		</div>
-
-
 		<button type="submit">valider</button>
+		
 	</form>
+	<?php } ?>
+	
+
+
+
