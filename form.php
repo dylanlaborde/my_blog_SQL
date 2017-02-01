e552c7552a2dd3afec51fd542d2a08f58dc3d0d3
Dylan <dylan4047@gmail.com>
2017-02-01 15:51:43 +0100

    creation du formulaire

--- form.php
+++ form.php
@@ -1,1 +1,19 @@
 <h1>form</h1>
+<form action="/submit_post.php" method="POST">
+	<div>
+		<div>
+			<label for="title">titre</label>
+			<input type="text" name="title">
+		</div>
+		<div>
+			<label for="author">auteur</label>
+			<input type="text" name="author">
+		</div>
+		<label for="content">contenu</label>
+		<div>
+			<textarea name="content" id="content" cols="30" rows="10"></textarea>
+		</div>
+
+
+		<button type="submit">valider</button>
+	</form>
